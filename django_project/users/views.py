09406813e637178b9bafc63f0e0506c995359a27
Stephanie Eigobhor <stefanyben02@gmail.com>
2022-11-07 21:26:05 +0100

    modified services.html, modified settings.py, modified views.py

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -8,7 +8,7 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            return redirect('legally-home')
+            return redirect('login')
     else:
         form = UserRegisterForm()
         return render(request, 'users/register.html', {'form': form})
